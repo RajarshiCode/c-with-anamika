2f3e91cc80c56089ed46585e03929b836b8434fe
RajarshiCode <rajarshidas47@gmail.com>
2024-12-26 21:21:37 +0530

    check check check

---
+++ Class 1/main.c
@@ -0,0 +1,26 @@
+#include<stdio.h> // header file
+
+int main(){
+
+    //memory sizes of the data types
+    printf("int %d byte\n", sizeof(int));
+    printf("float %d byte\n", sizeof(float));
+    printf("double %d byte\n", sizeof(double));
+    printf("char %d byte\n", sizeof(char));
+    printf("long long %d byte\n", sizeof(long long));
+
+
+    //data types
+    int a, b, c;  // variable declaration
+    a = 0; // assignment  
+    //float, char, double, long; 
+    
+        
+
+}
+// 1 byte = 8 bits
+// 1 byte = 2 nibbles
+// 1 KB(kilobytes) = 1024 bytes
+// 1 MB(MegaByte) = 1024Kb
+// 1 GB(GigaByte) = 1024mb
+// 1 TB(TeraByte) = 1024gb
