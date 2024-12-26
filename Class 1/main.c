3d8a603953b0eb544899eb522b0a81149ba962f9
RajarshiCode <rajarshidas47@gmail.com>
2024-12-26 22:03:52 +0530

    done and dusted!

--- Class 1/main.c
+++ Class 1/main.c
@@ -3,21 +3,43 @@
 int main(){
 
     //memory sizes of the data types
-    printf("int %d byte\n", sizeof(int));
-    printf("float %d byte\n", sizeof(float));
-    printf("double %d byte\n", sizeof(double));
-    printf("char %d byte\n", sizeof(char));
-    printf("long long %d byte\n", sizeof(long long));
+    // printf("int %d byte\n", sizeof(int));
+    // printf("float %d byte\n", sizeof(float));
+    // printf("double %d byte\n", sizeof(double));
+    // printf("char %d byte\n", sizeof(char));
+    // printf("long long %d byte\n", sizeof(long long));
 
 
     //data types
-    int a, b, c;  // variable declaration
-    a = 0; // assignment  
+    // int a, b, c;  // variable declaration
+    // a = 0; // assignment  
     //float, char, double, long; 
-    
-        
 
+    //return types
+    /*
+        // float, double, long long, int , char
+        int sum(int a, int b){
+            int sum = a+b;
+            return sum;        
+        }    
+    */
+
+   //keywords
+   /*
+        reserved words that has a special meaning for the c compiler.
+        naming of variables and functions.
+
+        for, while, int double float char long goto static auto signed unsigned
+        //int include;    
+    */
+
+   /*
+    dennis ritchie,  bell labs
+    1972 for the development of the UNIX OS   
+    */
 }
+
+// memory sizes
 // 1 byte = 8 bits
 // 1 byte = 2 nibbles
 // 1 KB(kilobytes) = 1024 bytes
