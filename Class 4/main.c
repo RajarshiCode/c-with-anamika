b51db41b62509f81848280179e9fd3c43f8c5cea
RajarshiCode <rajarshidas47@gmail.com>
2025-01-06 21:23:13 +0530

    Day 4 ends here, done and dusted!

---
+++ Class 4/main.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+int main(){
+    /*
+
+
+        int a = 5;
+        a++ // print 5 and then increment
+        ++a // increment 5 and then print
+
+
+        post and pre, increment and decrement
+
+        int a=11, b=22, c;         
+        c = a + b + a++ + b++ + ++a + ++b;
+        c = 11 + 22 + 11 + 22 + 13 + 24 = 
+
+        int c = 5, d = 8;
+        ans = c + d + c++ + d++ + d++ + ++d + ++c;
+        ans = 5 + 8 + 5 + 8 + 9 + 10 + 7
+
+        int k = 15, l = 30
+        ans = k++ + k + l++ + l++ + l++ + ++l + ++k
+        ans = 15 + 16 + 30 + 31 + 32 + 34 + 17
+
+
+        i = 0
+        while(i<4){
+            //something
+            i++, i+1;
+        }// 10 - 20 , if i = 0;
+
+        while(i<=20){
+            if(){
+            
+            }i++;
+        }
+        i = 0;
+        do{
+            //something
+        }while(i<4);     
+
+        
+    
+    */
+    return 0;
+}
