233e5e6a9f61605d7d5faaeceb33d41febf76619
RajarshiCode <rajarshidas47@gmail.com>
2025-01-16 21:01:25 +0530

    given some homework!

---
+++ Class 7/hw.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    /*
+        1. check how many no. are not divisible under n.
+        2. print " 10 multiplied by 10 gives you 100 "
+        3. print 1 2 3 4 5 6 in new lines
+        4. check how many no. are  divisible under n.
+        5. calculate sum and product upto 5.
+    
+    */
+    return 0;
+}   
